68448461403c45424605c67fca0e8741b68123fd
wook kim <wooke2e@gmail.com>
2017-06-05 13:11:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function getFirstSelector(selector){
   const lis = document.querySelector(selector)
-  return lis  
+  return lis
 }
 
 function nestedTarget(){
   const lis = document.querySelector('div#nested div.target')
+  return lis
+}
+
+function deepestChild(n){
+  
+
+
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('ul.ranked-list')
+  for (var i=0; i < lis.length; i++){
+    lis[i].innerHTML = (i + n).toString()
+  }
   return lis
 }
